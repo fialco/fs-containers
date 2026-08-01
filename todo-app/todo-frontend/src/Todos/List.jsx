2569c96b00e23df85f39c1fa7c99d62d579f8966
Fialco <113672867+Fialco@users.noreply.github.com>
2026-08-01 15:01:29 +0300

    Install and run prettier to frontend, refactor Todo

--- todo-app/todo-frontend/src/Todos/List.jsx
+++ todo-app/todo-frontend/src/Todos/List.jsx
@@ -1,4 +1,37 @@
-import React from 'react'
+const Todo = ({ todo, onClickDelete, onClickComplete }) => {
+  const doneInfo = (
+    <>
+      <span>This todo is done</span>
+      <span>
+        <button onClick={onClickDelete(todo)}> Delete </button>
+      </span>
+    </>
+  )
+
+  const notDoneInfo = (
+    <>
+      <span>This todo is not done</span>
+      <span>
+        <button onClick={onClickDelete(todo)}> Delete </button>
+        <button onClick={onClickComplete(todo)}> Set as done </button>
+      </span>
+    </>
+  )
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        maxWidth: '70%',
+        margin: 'auto',
+      }}
+    >
+      <span>{todo.text}</span>
+      {todo.done ? doneInfo : notDoneInfo}
+    </div>
+  )
+}
 
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
   const onClickDelete = (todo) => () => {
@@ -11,37 +44,15 @@ const TodoList = ({ todos, deleteTodo, completeTodo }) => {
 
   return (
     <>
-      {todos.map(todo => {
-        const doneInfo = (
-          <>
-            <span>This todo is done</span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-            </span>
-          </>
-        )
-
-        const notDoneInfo = (
-          <>
-            <span>
-              This todo is not done
-            </span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-              <button onClick={onClickComplete(todo)}> Set as done </button>
-            </span>
-          </>
-        )
-
-        return (
-          <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
-            <span>
-              {todo.text} 
-            </span>
-            {todo.done ? doneInfo : notDoneInfo}
-          </div>
-        )
-      }).reduce((acc, cur) => [...acc, <hr />, cur], [])}
+      {todos
+        .map((todo) => (
+          <Todo
+            todo={todo}
+            onClickDelete={onClickDelete}
+            onClickComplete={onClickComplete}
+          />
+        ))
+        .reduce((acc, cur) => [...acc, <hr />, cur], [])}
     </>
   )
 }
