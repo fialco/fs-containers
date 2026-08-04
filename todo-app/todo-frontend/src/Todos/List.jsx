b22dd4cd6462985483a73fcd1211bf3a2bcd1fcd
Fialco <113672867+Fialco@users.noreply.github.com>
2026-08-04 20:06:42 +0300

    Ex 20

--- todo-app/todo-frontend/src/Todos/List.jsx
+++ todo-app/todo-frontend/src/Todos/List.jsx
@@ -1,11 +1,11 @@
 import Todo from './Todo'
 
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
-  const onClickDelete = (todo) => () => {
+  const onClickDelete = (todo) => {
     deleteTodo(todo)
   }
 
-  const onClickComplete = (todo) => () => {
+  const onClickComplete = (todo) => {
     completeTodo(todo)
   }
 
